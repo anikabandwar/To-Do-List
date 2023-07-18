09e1efbb66016a9634692480a2a320019a61085f
Anika Bandwar <bandwar.anika@gmail.com>
2023-07-18 10:50:51 -0400

    Initial Commit

---
+++ To Do List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  To Do List
+//
+//  Created by Anika on 7/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
